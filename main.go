ae16fbc03608c525f47f15106d661621bcb1a1b4
Shiyang Wang <shiyangwang@umass.edu>
2021-12-22 20:52:28 -0500

    update to add delete and get users

--- main.go
+++ main.go
@@ -12,18 +12,37 @@ import (
 
 var addr = flag.String("addr", "127.0.0.1:8000", "http service address")
 
-type User struct {
-	Name       string
-	Age        int
-	RoomNumber int //fixme: this should not be int
-	Gender     int
-	DeviceID   string
-	DeviceType string
+var httpPrefix = "http://"
+
+var basicAuthUser = "test"
+var basicAuthPass = "test"
+
+type UserStruct struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
-func UserPost(URL string, user string, passwd string, data []byte) (*http.Response, error) {
+type PatientPageList struct {
+	Count int `json:"count"`
+	//Next     int       `json:"next"`
+	//Previous int       `json:"previous"`
+	Results []Patient `json:"results"`
+}
+
+type Patient struct {
+	Name       string     `json:"name"`
+	Age        int        `json:"age"`
+	RoomNumber int        `json:"room_no"` //fixme: this should not be int
+	Gender     string     `json:"gender"`
+	DeviceID   string     `json:"device_id"`
+	DeviceType string     `json:"device_type"`
+	User       UserStruct `json:"user"`
+}
+
+func userPost(URL string, user string, passwd string, data []byte) (*http.Response, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", URL, bytes.NewReader(data))
+	req, err := http.NewRequest(http.MethodPost, URL, bytes.NewReader(data))
 	req.SetBasicAuth(user, passwd)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
@@ -36,13 +55,57 @@ func UserPost(URL string, user string, passwd string, data []byte) (*http.Respon
 
 	return resp, err
 }
+
+func getUserList() []Patient {
+	client := &http.Client{}
+	URL := httpPrefix + *addr + "/seniors/"
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	req.SetBasicAuth(basicAuthUser, basicAuthPass)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	var pList PatientPageList
+	err = json.Unmarshal(bodyText, &pList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pList.Results
+}
+
+func deleteUser(deviceID string) {
+	client := &http.Client{}
+	URL := httpPrefix + *addr + "/seniors/" + deviceID + "/" //https://docs.djangoproject.com/en/2.2/ref/settings/#append-slash
+	fmt.Println(URL)
+
+	req, err := http.NewRequest(http.MethodDelete, URL, nil)
+	req.SetBasicAuth(basicAuthUser, basicAuthPass)
+	//req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Read Response Body
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(respBody))
+}
+
 func createUser() {
-	userData, err := json.Marshal(User{Name: "John Doe", Age: 31, RoomNumber: 1, Gender: 0, DeviceID: "7C1A23F227B4", DeviceType: "RRI"})
+	userData, err := json.Marshal(Patient{Name: "John Doe1", Age: 31, RoomNumber: 1, Gender: "M", DeviceID: "7C1A23F227B4", DeviceType: "RRI", User: UserStruct{Username: "test88", Email: "test@test.com", Password: "test23"}})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp, err := UserPost("http://"+*addr+"/seniors/", "test", "test", userData)
+	resp, err := userPost(httpPrefix+*addr+"/seniors/", basicAuthUser, basicAuthPass, userData)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -56,7 +119,13 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
-	createUser()
+	userList := getUserList()
+	for _, val := range userList {
+		fmt.Println("deleting user: ", val)
+		deleteUser(val.DeviceID)
+	}
+
+	//createUser()
 
 	//interrupt := make(chan os.Signal, 1)
 	//signal.Notify(interrupt, os.Interrupt)
